b1ffd240d2f467a5100993f0d455aa3b80a2b46d
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-10-17 17:29:52 +0300

    Change INSERT IGNORE to INSERT for Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,4 @@
 -- Insert single row into Customers table (ignore if duplicate)
-INSERT IGNORE INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
