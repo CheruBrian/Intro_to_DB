cbfcd3a6a04b912fd193f74fa1ae4d28c22b0aad
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-10-17 17:28:33 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
--- Insert single row into Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Insert single row into Customers table (ignore if duplicate)
+INSERT IGNORE INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
